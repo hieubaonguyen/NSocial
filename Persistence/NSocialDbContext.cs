d69b461d66dd07b25c481b6fca1771bb874af1fd
NguyenHieuBao <nnguyenhieubao@gmail.com>
2021-06-09 16:15:02 +0700

    Initial Structure Project

---
+++ Persistence/NSocialDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class NSocialDbContext : DbContext
+    {
+        public NSocialDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Value> values { get; set; }
+
+    }
+}
